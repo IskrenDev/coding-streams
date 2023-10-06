06b9e9aa810f7fd4518ae39edf431cb334d8e8a2
iskrenradev <iskrenr005@gmail.com>
2023-10-06 16:49:56 +0200

    Bonus task, subtasks 1, 2 and 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,23 +23,31 @@ public class Main {
         sortNumbers(numbers);
         sumNumbers(numbers);
         printAndAddNumbersToList(numbers);
+
+        try (Stream<String> lines = Files.lines(Paths.get("students.csv"))) {
+            List<String> studentsFromStream = lines.skip(1)
+                    .filter(line -> line != null).toList();
+            System.out.println(studentsFromStream);
+        } catch (IOException e) {
+            System.out.println("Can't find the file");
+        }
     }
 
     public static void filterEvenNumbers(List<Integer> numbers) {
         List<Integer> filteredNumbers = numbers.stream().filter(number -> number % 2 == 0)
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(filteredNumbers);
     }
 
     public static void multiplyNumbers(List<Integer> numbers) {
         List<Integer> multipliedNumbers = numbers.stream().map(number -> number * 2)
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(multipliedNumbers);
     }
 
     public static void sortNumbers(List<Integer> numbers) {
         List<Integer> sortedNumbers = numbers.stream().sorted()
-                .collect(Collectors.toList());
+                .collect(toList());
         System.out.println(sortedNumbers);
     }
 
@@ -38,6 +55,7 @@ public class Main {
         int sum = numbers.stream().parallel().reduce(0, (a, b) -> a + b);
         System.out.println(sum);
     }
+
     public static void printAndAddNumbersToList(List<Integer> numbers) {
         List<Integer> newNumbers = numbers.stream()
                 .peek(number -> System.out.println(number))
